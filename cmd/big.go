a9e87f427998d6269d276a81e9bdaff9ec3c8fa4
xztaityozx <taityonohibi@gmail.com>
2019-01-15 21:03:37 +0900

    add big sub command

---
+++ cmd/big.go
@@ -0,0 +1,97 @@
+// Copyright © 2019 xztaityozx
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+var AA = `
+          了                了了了
+          終終了            了終了
+        了終終              終終了
+      了終終了            了終終了了了了終終了
+      終終終    了了      了終終終終終終終終終了
+  了了終終了  了終終了  了終終了了了了了終終終
+了終終終終了了終終了    了終終終      了終終了
+了終終終終了了終了    了終終了終了  了終終終
+  了終終終終終終了  了終終了了終終了終終終了
+    了終終終終了了  了終了    了終終終終了
+      了終終了了終了  了        了終終終
+      了終終    終終了          了終終了了
+了了了終終終了終終終終了    了了終終終終終了
+終終終終終終終終了終終了了終終終終了終終終終終終了了
+終終了了了終終    了了終終終終了了    了終終終終終了
+了了了    終終  了了了終終終了了終了了了了終終終終了
+  終終終了終終了終終了終終了  了終終終終了了了了了
+  終終終了終終了終終了了了    了終終終終終終了
+  終終終了終終  了終終          了了終終終終了
+了終終終  終終  了終終了    了      了終終了
+了終終終  終終    終終了  了終終了了了
+了終終了  終終    終終了  了終終終終終了了
+了終終了  終終    了了    了終終終終終終終終了了
+了終終了  終終              了了終終終終終終終了
+          終終                    了終終終終終了
+          終終                        了了了了
+`
+
+// bigCmd represents the big command
+var bigCmd = &cobra.Command{
+	Use:   "big",
+	Short: "デカい終を出しますよ",
+	Long:  fmt.Sprintf("%s\n\nを出力します\n引数を与えると，終了の文字がそれになります\n", AA),
+	Run: func(cmd *cobra.Command, args []string) {
+		offset, _ := cmd.Flags().GetInt("offset")
+
+		PrintBig(strings.Join(args, ""), offset)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bigCmd)
+	bigCmd.Flags().Int("offset", 0, "左端からの距離です")
+}
+
+func PrintBig(text string, offset int) {
+	status := len(text) == 0
+	runeText := []rune(text)
+	textIdx := 0
+	maxLength := len(runeText)
+	for _, line := range strings.Split(AA, "\n") {
+		if status {
+			PaddingPrint(line, offset)
+		} else {
+			var replaced []rune
+			for _, c := range []rune(line) {
+				if c == rune('終') || c == rune('了') {
+					replaced = append(replaced, runeText[textIdx])
+					textIdx = (textIdx + 1) % maxLength
+				} else {
+					replaced = append(replaced, rune(' '))
+				}
+			}
+
+			PaddingPrint(string(replaced), offset)
+		}
+	}
+}
