5152bd64cada75c39918296d9075c2e8abbb2465
xztaityozx <taityonohibi@gmail.com>
2019-06-02 23:13:09 +0900

    #7 楽しい休日の終わりを追加したよ

---
+++ cmd/funnySunday.go
@@ -0,0 +1,53 @@
+// Copyright © 2019 xztaityozx
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// funnySundayCmd represents the funnySunday command
+var funnySundayCmd = &cobra.Command{
+	Use:     "funnySunday",
+	Aliases: []string{"fs", "fun"},
+	Short:   "楽しい休日は終わった",
+	Run: func(cmd *cobra.Command, args []string) {
+		aa := `たのしい休日
+
+                           /||￣￣|| ∧∧
+                          l ||＿＿||(,/⌒ヽ
+                         [l  | ,,― とﾉ    ヽ
+                    |￣￣￣￣￣     (＿＿＿ )
+
+                                          ┼ヽ   -|r‐､.  ﾚ  |
+                                         ｄ⌒)  ./| _ﾉ    __ﾉ`
+
+		offset, _ := cmd.Flags().GetInt("offset")
+		PrintAA(strings.Split(aa, "\n"), offset)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(funnySundayCmd)
+	funnySundayCmd.Flags().Int("offset", 0, "左側からの距離です")
+}
