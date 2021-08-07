c2db785d67eeb9b744d741656ce81bfd913e640c
xztaityozx <taityonohibi@gmail.com>
2021-08-07 19:09:31 +0900

    一番シンプルな終わりのテスト

--- aa/arts/simple_test.go
+++ aa/arts/simple_test.go
@@ -1,10 +1,30 @@
-package arts_test
+package arts
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
 
-func TestSimpleOwari_Print(t *testing.T) {
+func TestNewSimpleOwari(t *testing.T) {
+	so := NewSimpleOwari("text", "author")
 
+	assert.Equal(t, "text", so.text)
+	assert.Equal(t, "author", so.author)
 }
 
 func TestSimpleOwari_Load(t *testing.T) {
+	so := NewSimpleOwari("", "")
+
+	err := so.Load("")
+	assert.Nil(t, err)
+
+	expect := []string{
+		"", "",
+		"        糸冬",
+		"---------------------",
+		"  制作・著作 ＮＨＫ",
+	}
+
+	assert.Equal(t, strings.Join(expect, "\n"), strings.Join(so.AsciiArt, "\n"))
 }
