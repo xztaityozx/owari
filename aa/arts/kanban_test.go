3acacb07a381c77075118da684a62cf92be5046c
xztaityozx <taityonohibi@gmail.com>
2021-08-14 16:22:18 +0900

    看板

---
+++ aa/arts/kanban_test.go
@@ -0,0 +1,185 @@
+package arts
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestNewKanban(t *testing.T) {
+	k := NewKanban([]string{"text"})
+	assert.NotNil(t, k)
+	assert.False(t, k.giko)
+	assert.False(t, k.reverse)
+	assert.False(t, k.konata)
+	assert.False(t, k.twinGiko)
+	assert.Equal(t, []string{"text"}, k.text)
+}
+
+func TestKanban_Load(t *testing.T) {
+	t.Run("装飾がないやつ", func(t *testing.T) {
+		k := NewKanban(nil)
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣|",
+			"|        終        |",
+			"|    制作・著作    |",
+			"|  ￣￣￣￣￣￣￣  |",
+			"|     Ｎ Ｈ Ｋ     |",
+			"|＿＿＿＿＿＿＿＿＿|",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("text1行", func(t *testing.T) {
+		k := NewKanban([]string{"おーーーーーーーーーーーーわり！"})
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|",
+			"|  おーーーーーーーーーーーーわり！  |",
+			"|             制作・著作             |",
+			"|  ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣  |",
+			"|              Ｎ Ｈ Ｋ              |",
+			"|＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("text2行", func(t *testing.T) {
+		k := NewKanban([]string{
+			"おーーーーーーーーーーーーわり！",
+			"おーーーーーーーーーーーーわり！",
+		})
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|",
+			"|  おーーーーーーーーーーーーわり！  |",
+			"|  おーーーーーーーーーーーーわり！  |",
+			"|             制作・著作             |",
+			"|  ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣  |",
+			"|              Ｎ Ｈ Ｋ              |",
+			"|＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿|",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("giko", func(t *testing.T) {
+		k := NewKanban([]string{"終"})
+		k.SetGikoNeko(true)
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣|",
+			"|        終        |",
+			"|    制作・著作    |",
+			"|  ￣￣￣￣￣￣￣  |",
+			"|     Ｎ Ｈ Ｋ     |",
+			"|＿＿＿＿＿＿＿＿＿|",
+			"  ∧∧   ||         ",
+			"  ( ﾟдﾟ)||           ",
+			"  /    づΦ         ",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("twin giko", func(t *testing.T) {
+		k := NewKanban([]string{"終"})
+		k.SetTwinGikoNeko(true)
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣|",
+			"|        終        |",
+			"|    制作・著作    |",
+			"|  ￣￣￣￣￣￣￣  |",
+			"|     Ｎ Ｈ Ｋ     |",
+			"|＿＿＿＿＿＿＿＿＿|",
+			"  ∧∧   ||  ∧∧   ",
+			"  ( ﾟдﾟ)||(ﾟдﾟ )      ",
+			"  /    づΦ⊂    \\  ",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("giko reverse", func(t *testing.T) {
+		k := NewKanban([]string{"終"})
+		k.SetGikoNeko(true)
+		k.SetReverse(true)
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣|",
+			"|        終        |",
+			"|    制作・著作    |",
+			"|  ￣￣￣￣￣￣￣  |",
+			"|     Ｎ Ｈ Ｋ     |",
+			"|＿＿＿＿＿＿＿＿＿|",
+			"         ||  ∧∧   ",
+			"         ||(ﾟдﾟ )    ",
+			"         Φ⊂    \\  ",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("konata", func(t *testing.T) {
+		k := NewKanban([]string{"終"})
+		k.SetKonata(true)
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣|",
+			"|        終        |",
+			"|    制作・著作    |",
+			"|  ￣￣￣￣￣￣￣  |",
+			"|     Ｎ Ｈ Ｋ     |",
+			"|＿＿＿＿＿＿＿＿＿|",
+			" ∧ ∧   ||         ",
+			"(≡ω≡.)||         ",
+			"/      づΦ         ",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+
+	t.Run("custom author", func(t *testing.T) {
+		k := NewKanban([]string{"終"})
+		k.SetAuthor("TEST")
+		assert.NotNil(t, k)
+		err := k.Load("default")
+		assert.Nil(t, err)
+
+		expect := []string{
+			"|￣￣￣￣￣￣￣￣￣|",
+			"|        終        |",
+			"|    制作・著作    |",
+			"|  ￣￣￣￣￣￣￣  |",
+			"|       TEST       |",
+			"|＿＿＿＿＿＿＿＿＿|",
+		}
+
+		assert.Equal(t, strings.Join(expect, "\n"), strings.Join(k.AsciiArt, "\n"))
+	})
+}
